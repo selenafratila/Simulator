f76ca587678a00d7becb5cede336b3bd8199868b
selenafratila <selenafratila225@gmail.com>
2026-03-14 23:31:39 +0200

    Finalized HetNet simulation

---
+++ simulator.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+print("--- TEST CONFIGURARE ---")
+print(f"Salut, Selena! Python functioneaza.")
+print(f"Versiune: {sys.version}")
+print(f"Folder proiect: {os.getcwd()}")
+print("------------------------")
